a5549102c2a6a0bcc019c27081dc7e540f2d5ce6
jlmonroyhdez <jlmonroyhdez@gmail.com>
2015-01-04 00:24:35 -0600

    first commit

---
+++ app/src/main/java/com/mexico/jlmonroyhdez/test2gridview/MainActivity.java
@@ -0,0 +1,49 @@
+package com.mexico.jlmonroyhdez.test2gridview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void TestCvs1(){
+        String[] = {"wew","sdsdas","sdadas"};
+        for (int i = 0; i < FOCUSED_STATE_SET.length; i++) {
+            int i1 = FOCUSED_STATE_SET[i];
+
+        }
+
+        }
+    }
+}
